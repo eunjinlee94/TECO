a0fdb67ca970eb9c7bd19f282bb9d379cf86ad69
jsh0924 <jeongsh0924@gmail.com>
2025-04-29 17:08:01 +0900

    250429-5

--- church-homepage/src/App.js
+++ church-homepage/src/App.js
@@ -3,6 +3,8 @@ import Join from './pages/Join';
 import Login from './pages/Login';
 import Index from './pages/Index';
 import Church1 from './pages/Church1';
+import Church2 from './pages/Church2';
+import Church3 from './pages/Church3';
 import Header from './components/Header';
 import Footer from './components/Footer'; // ★ フッター追加
 import './App.css';
@@ -20,6 +22,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           {/* 他のページもここに追加可能 */}
           <Route path="/church1" element={<Church1 />} />
+          <Route path="/church2" element={<Church2 />} />
+          <Route path="/church3" element={<Church3 />} />
         </Routes>
       </div>
 
